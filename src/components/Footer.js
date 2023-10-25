c91c7eeeb58079bbf068f8d816020db9e2fbefd1
Harry Crawford <harry.crawford789@hotmail.co.uk>
2023-10-25 11:50:38 +0200

    Added link to home for clicks on the Attendence Manager title in the navigation bar. Also fixed a ReactJS warning of multiple occurences of the same css property in src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-  let style = {
-    position: 'relative',
-    bottom: '0',
-    bottom: "0px",
-    padding: ".5em",
-    width: "100%",
-    textAlign: "center",
-    backgroundColor: "#6246ea",
-    color: "#fffffe",
-    fontWeight: "bold"
-  }
-  return (
-    <footer>
-      <p style={style}>Made By Anshul Jain</p>
-    </footer>
-  )
+    let style = {
+        position: "relative",
+        bottom: "0",
+        padding: ".5em",
+        width: "100%",
+        textAlign: "center",
+        backgroundColor: "#6246ea",
+        color: "#fffffe",
+        fontWeight: "bold",
+    };
+    return (
+        <footer>
+            <p style={style}>Made By Anshul Jain</p>
+        </footer>
+    );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,27 @@
-import "./Navbar.css"
+import "./Navbar.css";
 export default function Navbar(props) {
+    const handleLogout = () => {
+        localStorage.removeItem("attendenceData");
+        window.location.reload();
+    };
 
-
-  const handleLogout = ()=>{
-
-    localStorage.removeItem('attendenceData')
-    window.location.reload()
-
-
-  }
-
-  return (
-    <div className='navbar'>
-      <div>
-        <h3>Attendence Manager</h3>
-      </div>
-      <div className='options'>
-        <button onClick={()=>props.setnav("/")}>Home</button>
-        {props.data?<button onClick={()=>props.setnav("add")}>Add Subject</button>:""}
-        <button onClick={()=>props.setnav("about")}>About</button>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="navbar">
+            <a href="/">
+                <h3>Attendence Manager</h3>
+            </a>
+            <div className="options">
+                <button onClick={() => props.setnav("/")}>Home</button>
+                {props.data ? (
+                    <button onClick={() => props.setnav("add")}>
+                        Add Subject
+                    </button>
+                ) : (
+                    ""
+                )}
+                <button onClick={() => props.setnav("about")}>About</button>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+        </div>
+    );
 }
